22b19a904c9bcdb01b43dcd067a60c3823dde5a0
Svyatoslav Krasnov <beatcazzze@gmail.com>
2020-06-04 23:38:47 +0300

    CORS Policy additions to app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 
-const { SERVER_PORT, DATABASE_URI, RATE_LIMITER } = require('./config');
+const { SERVER_PORT, DATABASE_URI, RATE_LIMITER, ALLOWED_ORIGINS } = require('./config');
 const { errorMiddleware } = require('./middlewares');
 const router = require('./routes');
 
@@ -29,6 +29,17 @@ app.use(rateLimit(RATE_LIMITER));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// configuring CORS
+app.use('*', function(req, res, next) {
+  const { origin } = req.headers;
+  if (ALLOWED_ORIGINS.includes(origin)) res.header('Access-Control-Allow-Origin', origin);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') res.status(204).send('OK');
+  else next();
+})
 
 // adding main router
 app.use(router);
